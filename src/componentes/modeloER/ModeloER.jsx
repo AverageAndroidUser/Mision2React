1861a92d634f8141fe67ad9e1688d8b3e5991baa
AverageAndroidUser <juandiegoascanio13@gmail.com>
2025-06-28 16:02:48 -0500

    ModeloER&Home

--- src/componentes/modeloER/ModeloER.jsx
+++ src/componentes/modeloER/ModeloER.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React from 'react';
+import styles from './ModeloER.module.css';
 
 export default function ModeloER() {
   return (
-    <div>
-      <h2>IEGresos</h2>
-      <h3>Diagrama de Clases</h3>
-      <img src = "https://res.cloudinary.com/dpuixn4lq/image/upload/v1751117894/IEGresos_eourck.png"></img>
-      <p>Permite a los usuarios registrar, visualizar y gestionar ingresos y egresos de manera sencilla. 
-        El sistema clasifica las transacciones, permite su edición y eliminación, 
-        y muestra un resumen con el saldo actual.</p>
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <h2 className={styles.title}>IEGresos</h2>
+        <h3 className={styles.subtitle}>Diagrama de Clases</h3>
+        <div className={styles.content}>
+          <img
+            className={styles.image}
+            src="https://res.cloudinary.com/dpuixn4lq/image/upload/v1751117894/IEGresos_eourck.png"
+            alt="Diagrama de Clases"
+          />
+          <p className={styles.text}>
+            Permite a los usuarios registrar, visualizar y gestionar <strong className={styles.ieText}>ingresos</strong> y <strong className={styles.ieText}>egresos</strong> de manera sencilla.
+            El sistema clasifica las transacciones, permite su edición y eliminación,
+            y muestra un resumen con el saldo actual.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+
